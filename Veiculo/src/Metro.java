0d1a19bbf22c4e71babbe6146424574015167db4
Esther <esther.cardoso.silva@gmail.com>
2016-04-11 01:12:53 -0300

    atualização 11/04.
    Assinatura das Classes a serem implementadas.
    Interfaces: "VeiculoTransporteCarga" e "VeiculoTransportePassageiros".

---
+++ Veiculo/src/Metro.java
@@ -0,0 +1,5 @@
+
+public final class Metro extends Veiculo implements VeiculoTransportePassageiros
+{
+
+}
---
+++ Veiculo/src/OnibusInterurbano.java
@@ -0,0 +1,5 @@
+
+public final class OnibusInterurbano extends Veiculo implements VeiculoTransportePassageiros
+{
+
+}
